964355899860c9efc6c05948f63ca088144f6ed6
arrjon <jonas.arruda@uni-bonn.de>
2025-12-02 20:05:00 +0100

    update gitignore

--- install_requirements.py
+++ install_requirements.py
@@ -20,7 +20,6 @@ packnames = ('dplyr', 'stringr', 'actuar')
 from rpy2.robjects.vectors import StrVector
 
 # Selectively install what needs to be installed
-# We are fancy, just because we can.
 names_to_install = [x for x in packnames if not rpackages.isinstalled(x)]
 if len(names_to_install) > 0:
     utils.install_packages(StrVector(names_to_install))
